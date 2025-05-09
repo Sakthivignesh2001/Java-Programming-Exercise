56976500c075f0cf0b2bcc04029de15058d2d2ec
sathi <sathi@DESKTOP-9B7RA86.lan>
2025-05-09 19:04:48 +0530

    [Basic star pattern programming][9/5/25]

---
+++ 1.Pattern Programming in Java/src/com/example/star/basic/Pattern2.java
@@ -0,0 +1,32 @@
+package com.example.star.basic;
+
+/*
+ * 
+ * 	Print 5 * 5 only outside line
+ * 
+ * i -> row(left to right)
+ * j -> column (top to bottom)
+ * 
+ */
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		
+		int n = 5;
+		
+		for(int i = 0 ; i < n ; i++) {
+			for(int j = 0 ; j < n ; j++) {
+				if(i == 0 || j == 0 || i == n-1 || j == n-1) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+
+	}
+
+}
---
+++ 1.Pattern Programming in Java/src/com/example/star/basic/Pattern3.java
@@ -0,0 +1,35 @@
+package com.example.star.basic;
+
+/*
+ * 
+ * 	Print H
+ * 
+ * i -> row(left to right)
+ * j -> column (top to bottom)
+ * 
+ * (n-1)/2 -> center line
+ * 
+ */
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		
+		int n = 5;
+		
+		for(int i = 0 ; i < n ; i++) {
+			for(int j = 0 ; j < n ; j++) {
+				if(j == 0 || j == n-1 || i == (n-1)/2) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+
+
+	}
+
+}
