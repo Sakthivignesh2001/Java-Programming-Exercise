b997df5d7a31cbe56173930c5e5d6bdb2ef5a021
sathi <sathi@DESKTOP-9B7RA86.lan>
2025-05-19 21:37:28 +0530

    [Pattern Programming][19-05-25]

---
+++ 1.Pattern Programming in Java/src/com/example/star/alphabets/Pattern01.java
@@ -0,0 +1,84 @@
+package com.example.star.alphabets;
+
+public class Pattern01 {
+
+	public static void main(String[] args) {
+		
+		int n =10;
+		
+		for(int i = 0 ; i < n ; i++) {
+			
+			// A
+			for(int j = 0 ; j < n ; j++) {
+				if(
+					(i == 0 && j > 0 && j < n-1) ||
+					(j == 0 && i > 0 && i < n) ||
+					(j == n-1 && i > 0 && i < n) ||
+					(i == n/2)
+				) {
+					System.out.print("*");
+				} else{
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.print("    ");
+			
+			// B
+			for(int j = 0 ; j < n ; j++) {
+				if(
+					(j == 0) ||
+					(i == 0 && j < (3*n)/4) ||
+					(i == n-1 && j < (3*n)/4) ||
+					(i == n/2 && j < (3*n)/4) ||
+					(j == (3*n)/4 && i > 0 && i < n/2) ||
+					(j == (3*n)/4 && i > n/2 && i < n-1 )
+				) {
+					System.out.print("*");
+				} else{
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.print("    ");
+			
+			// C
+			for(int j = 0 ; j < n ; j++) {
+				if(
+					(i == 0 && j > 0) ||
+					(i == n-1 && j > 0) ||
+					(j == 0 && i > 0 && i < n-1)
+				) {
+					System.out.print("*");
+				} else{
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.print("    ");
+			
+			// D
+			for(int j = 0 ; j < n ; j++) {
+				if(
+					(j == 1) ||
+					(i == 0 && j < n-1) ||
+					(i == n-1 && j < n-1) ||
+					(j == n-1 && i > 0 && i < n-1)
+				) {
+					System.out.print("*");
+				} else{
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.print("    ");
+			
+			//E
+			
+			
+			System.out.println();
+		}
+
+	}
+
+}
